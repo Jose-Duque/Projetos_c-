ca9bc78db672e1ed1c207eb6e2b3b2a3b0e01719
Jose Carlos <cj_duque@hotamil.com>
2020-04-07 09:23:45 -0300

    Proj_Estudante

---
+++ Estudante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projeto_com_Git {
+    class Estudante {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Estudante(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString() {
+            return Nome + ", " + Email;
+        }
+
+    }
+}
